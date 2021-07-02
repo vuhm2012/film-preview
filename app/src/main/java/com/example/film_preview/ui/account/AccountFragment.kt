022b02a9a614ce789ddbab1c27f292f2f9809fd5
vuhm <hn.minhvu@gmail.com>
2021-07-02 16:03:28 +0700

    Update libraries + icons + bottom navigation bar

--- app/src/main/java/com/example/film_preview/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/film_preview/ui/account/AccountFragment.kt
@@ -1,13 +1,13 @@
-package com.example.film_preview.ui.setting
+package com.example.film_preview.ui.account
 
 import android.os.Bundle
 import android.view.View
 import com.example.film_preview.R
 import com.example.film_preview.base.BaseFragment
 
-class SettingFragment : BaseFragment() {
+class AccountFragment : BaseFragment() {
 
-    override fun getLayoutId(): Int = R.layout.fragment_setting
+    override fun getLayoutId(): Int = R.layout.fragment_account
 
     override fun initControls(view: View, savedInstanceState: Bundle?) {
 
